2114ec6cd8345bccb267838795c8eb8fae3dd93b
Ginny <imvkun@gmail.com>
2025-08-24 02:19:47 -0700

    Update script.js
    -Added comments
    -Removed enter script
    -heroNames are now all lowercase and ignore user input case when doing comparisons

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-setInterval(fetchHeroData, 2000);
+setInterval(fetchHeroData, 2000); // Refresh the information input every 2 seconds
 
+// PURPOSE: Take the information the player has input and calculate the speed of each hero
 async function fetchHeroData(){
     await fetch("List of Heroes.txt")
         .then(response => response.text())
         .then(text => {
-            // Hero Names Array
-            heroNames = text.split("\r\n");
+            heroNames = text.toLowerCase().split("\r\n"); // Hero Names Array
+
+            // Cycles each hero by number
             for(let i = 1; i <= 6; i++){
-                // Cycles each hero by number
                 let heroNum = "hero" + i;
                 let hero = document.getElementById(heroNum + "name").value; // Takes the hero's name by user input
 
                 // Makes sure that the hero name is valid and on the list of heroes before displaying icon
-                if(2 < hero.length && heroNames.includes(hero)){
+                // If not on the list, keeps default Keaton icon/assigns default Keaton icon
+                if(2 < hero.length && heroNames.includes(hero.toLowerCase())){
                     let heroPortrait = "https://epic7db.com/images/heroes/" + hero + ".webp"; // Gets hero portait
                     document.getElementById(heroNum + "icon").src = heroPortrait.replace(/\s/g, "-").toLowerCase(); // Displays hero portait to user
                 }else{
@@ -28,14 +30,4 @@ async function fetchHeroData(){
                 document.getElementById("calcCR" + i).textContent = rangeBot.toFixed(2) + " - " + rangeTop.toFixed(2);
             }
         });
-}
-
-
-
-// Check if the user pressed enter on Hero name
-function checkName(){
-    let input = document.getElementById("hero1name");
-    if(event.keyCode === 13){
-        fetchHeroData();
-    }
-}
+} // End async function fetchHeroData()
